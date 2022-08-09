beacd03ec35e7b9fc27d15df60dda33e7b4fec97
EtafHadda <etafabuhadda18@gmail.com>
2022-08-09 13:12:04 +0300

    edit utilities.py

--- utilities.py
+++ utilities.py
@@ -28,47 +28,38 @@ SPECIAL_CHARS = "[$&+,:;=?@#|'<>.^*()%!-]"
 def encode_single_log_line(log_line):
     log_line = log_line.replace(',','_')
     REGEX = '(\d+-\d+-\d+\w\d+:\d+:\d+.*\d+:\d+\s\w* \w+\:\s)([(\d\.)]+) - - \[(.*?)\] "(.*?)" (\d+) (.+) "(.*?)" "(.*?)"'
-    #REGEX = '([(\d\.)]+) - - \[(.*?)\] "(.*?)" (\d+) (.+) "(.*?)" "(.*?)"'
+    #REGEX = '([(\d\.)]+) - - \[(.*?)\] "(.*?)" (\d+) (.+) "(.*?)" "(.*?)"' # this regex is for logs without rsyslog prefix
 
     log_line = re.match(REGEX, log_line).groups()
-    '''
-    print(log_line[0])
-    print(log_line[1])
-    print(log_line[2])
-    print(log_line[3])
-    print(log_line[4])
-    print(log_line[5])
-    print(log_line[6])
-    print(log_line[7])
-    '''
-
-
-
     url = log_line[3]
     return_code = log_line[4]
-    param_number = len(url.split('&'))
-    url_length = len(url)
-    size = str(log_line[5]).rstrip('\n')
-    depth = sum(1 for c in url if c == '/')
-    upper_cases = sum(1 for c in url if c.isupper())
-    lower_cases = sum(1 for c in url if c.islower())
-    special_chars = sum(1 for c in url if c in SPECIAL_CHARS)
+    if url != "-":
+        url = log_line[3]
+        param_number = len(url.split('&'))
+        url_length = len(url)
+        size = str(log_line[5]).rstrip('\n')
+        depth = sum(1 for c in url if c == '/')
+        upper_cases = sum(1 for c in url if c.isupper())
+        lower_cases = sum(1 for c in url if c.islower())
+        special_chars = sum(1 for c in url if c in SPECIAL_CHARS)
 
-    if '-' in size:
-        size = 0
-    else:
-        size = int(size)
-    if (int(return_code) > 0):
-        log_line_data = {}
-        log_line_data['size'] = int(size)
-        log_line_data['param_number'] = int(param_number)
-        log_line_data['length'] = int(url_length)
-        log_line_data['return_code'] = int(return_code)
-        log_line_data['upper_cases'] = int(upper_cases)
-        log_line_data['lower_cases'] = int(lower_cases)
-        log_line_data['special_chars'] = int(special_chars)
-        log_line_data['depth'] = int(depth)
+        if '-' in size:
+            size = 0
+        else:
+            size = int(size)
+        if (int(return_code) > 0):
+            log_line_data = {}
+            log_line_data['size'] = int(size)
+            log_line_data['param_number'] = int(param_number)
+            log_line_data['length'] = int(url_length)
+            log_line_data['return_code'] = int(return_code)
+            log_line_data['upper_cases'] = int(upper_cases)
+            log_line_data['lower_cases'] = int(lower_cases)
+            log_line_data['special_chars'] = int(special_chars)
+            log_line_data['depth'] = int(depth)
 
+        else:
+            log_line_data = None
     else:
         log_line_data = None
     return url,log_line_data,return_code
